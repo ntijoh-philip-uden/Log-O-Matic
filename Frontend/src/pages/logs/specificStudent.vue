eec2279d694d10b201c140fd48b8a28ae35310d8
ntijoh-zejd-avdic <zejd.avdic@elev.ga.ntig.se>
2024-11-18 12:56:04 +0100

    Ska ändå kopiera från dag-elev-vy

--- Frontend/src/pages/logs/specificStudent.vue
+++ Frontend/src/pages/logs/specificStudent.vue
@@ -4,11 +4,85 @@
 
 
 <template>
+    <v-container>
+        <v-card
+          class="mx-auto"
+          max-width="900"
+          hover
+        >
+          <v-card-item>
+            <v-card-title>
+              Vad har du gjort under dagen?
+            </v-card-title>
+    
+            <v-card-subtitle>
+                <!-- blir en subtitle om man skriver här -->
+            </v-card-subtitle>
+    
+          </v-card-item>
 
-<h1>hejs</h1>
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+
+          <hr>
+
+          <v-card-item>
+            <v-card-title>
+              Vad har du lärt dig?
+            </v-card-title>
+    
+            <v-card-subtitle>
+                <!-- blir en subtitle om man skriver här -->
+            </v-card-subtitle>
+    
+          </v-card-item>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+          
+          <hr>
+
+          <v-card-item>
+            <v-card-title>
+              Vad förstod du inte / Vilka frågor har du inte fått svar på?
+            </v-card-title>
+    
+            <v-card-subtitle>
+                <!-- blir en subtitle om man skriver här -->
+            </v-card-subtitle>
+    
+          </v-card-item>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+
+          <hr>
+
+          <v-card-item>
+            <v-card-title>
+              Vad vill du lära dig mer om?
+            </v-card-title>
+    
+            <v-card-subtitle>
+                <!-- blir en subtitle om man skriver här -->
+            </v-card-subtitle>
+    
+          </v-card-item>
+
+          <v-card-text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </v-card-text>
+
+        </v-card>
+    </v-container>
+
+    
 
 
-</template>
+  </template>
 
   
 <style scoped>
